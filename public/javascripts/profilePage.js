a377f476687c140ed62bb35e12bb255e5008c15b
jaydee3 <jaydee3@users.noreply.github.com>
2019-08-27 19:35:42 -0700

    added Node.js modules and public folder

---
+++ public/javascripts/profilePage.js
@@ -0,0 +1,7 @@
+//UNUSED AS OF MOST RECENT COMMIT
+
+//These document elements are for select statements in profile.handlebars
+var stateSelect = document.getElementById("user_state_select");
+var genderSelect = document.getElementById("user_gender_select");
+var maritalSelect = document.getElementById("user_maritalStatus_select");
+var educationSelect = document.getElementById("user_education_select");
